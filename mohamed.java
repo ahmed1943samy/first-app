f4e592a2d7e18b6760cc424c1dfcc70e8e03cf04
ahmed1943samy <152991748+ahmed1943samy@users.noreply.github.com>
2023-12-05 21:06:43 +0200

    added code files

---
+++ mohamed.java
@@ -0,0 +1,8 @@
+public class mohamed {
+    public static void main(String[] args) {
+        System.out.println("*****-*****");
+        System.out.println("**your image is done");
+        System.out.println("*****-*****");
+
+    }
+}
